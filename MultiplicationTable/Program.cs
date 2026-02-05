5ceed3b18b325a96e6cac17a08d8cf590e202ebe
cambriajm <cambriajmorgan@gmail.com>
2026-02-04 20:01:50 -0700

    oh man that sucked. i had the order all weird then my computer died and I lost what i originally did. rip

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.ExceptionServices;
+﻿using System.Data.Common;
+using System.Runtime.ExceptionServices;
 
 namespace MultiplicationTable
 {
@@ -6,9 +7,62 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            int firstNumber, secondNumber;
             string userInput = "";
-            for (int row = 1; row = firstNumber)
+            
+            
+            {
+            Console.WriteLine("Welcome to the multiplication table," +
+                "enter the number you wish to see the times table for.");
+            
+                userInput = Console.ReadLine();
+                
+
+                if(userInput != null)
+                {
+                    try
+                    {
+
+                    for (int array = 0; array < 1; array++)
+                    {
+
+                        for (int row = 1; row < int.Parse(userInput) + 1; row++)
+                        {
+                            for
+                                (int column = 1; column < int.Parse(userInput) + 1; column++)
+                            {
+                                Console.Write((row * column).ToString().PadLeft(4));
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                        int result = int.Parse(userInput);
+
+                    }
+                    catch(Exception)
+                    {
+                        for (int array = 0; array < 1; array++)
+                        {
+
+                            for (int row = 1; row < 12 + 1; row++)
+                            {
+                                for
+                                    (int column = 1; column < 12 + 1; column++)
+                                {
+                                    Console.Write((row * column).ToString().PadLeft(4));
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                    }
+
+                //pause 
+                Console.Read();
+                            
+                    }
+                }
+
+            } 
+
         }
-    }
-}
+ 
